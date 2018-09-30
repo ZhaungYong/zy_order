62596d47b6dbcb7bb24a6822d9de4952dbcea70e
ZhuangYong <javaweb_zy@163.com>
2018-09-30 15:10:27 +0800

    edit

--- src/main/java/cn/zy/service/ItemService.java
+++ src/main/java/cn/zy/service/ItemService.java
@@ -4,20 +4,18 @@ import cn.zy.pojo.Item;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ItemService {
     @Autowired
-    private RestTemplate restTemplate;
+    private ItemFeignClient itemFeignClient;
 
     @HystrixCommand(fallbackMethod = "queryItemByIdFallbackMethod") // 进行容错处理
     public Item getItemById(String id) {
-        String serviceId = "zy-item";
-        return restTemplate.getForObject("http://" + serviceId + "/item/" + id, Item.class);
+        return itemFeignClient.getItemById(id);
     }
 
-    public Item queryItemByIdFallbackMethod(String id){ // 请求失败执行的方法
+    public Item queryItemByIdFallbackMethod(String id) { // 请求失败执行的方法
         return new Item(1L, "查询商品信息出错!", null, null, null);
     }
 }
